cac6ecb370c99311c4ecdd8e14fdb42f2698a2f4
Lillian Luzinsky <eyeris@tpg.com.au>
2020-07-02 08:57:22 +1000

    fixed hamburger animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,19 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
 
-let showMenu = false;
+let showMenu = false;//initialy the menu is not shown
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
     if(!showMenu){
         hamburger.classList.add('open');
+
         showMenu = true;
     } else {
         hamburger.classList.remove('open');
+
         showMenu = false;
     }
 }
+
